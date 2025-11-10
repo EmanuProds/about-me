be6d35fa7ab6917bd0f6b87230a770345eca5f41
EmanuProds <emanuprods@proton.me>
2025-11-10 14:21:02 -0300

    update header style

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const { t } = useTranslations();
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 p-4 bg-gray-200/60 dark:bg-black/20 text-gray-900 dark:text-gray-100 backdrop-blur-lg border-b border-white/20 dark:border-white/10 transition-colors duration-300">
+    <header className="fixed top-0 left-0 right-0 z-50 p-4 bg-gray-200/60 dark:bg-black/20 text-gray-900 dark:text-gray-100 backdrop-blur-lg border-b border-gray-400/20 dark:border-gray-100/20 transition-colors duration-300">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl font-semibold">@EmanuProds</h1>
 

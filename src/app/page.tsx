5c555bc0e1d58e84ba7d63f4df97a636b8632524
EmanuProds <emanuprods@proton.me>
2025-11-10 14:20:12 -0300

    add import tech scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/layout/Header";
 import Profile from "@/components/ui/Profile";
 import Footer from "@/components/layout/Footer";
+import TechScroll from "@/components/ui/TechScroll";
 
 export default function Home() {
   return (
     <main className="pt-20">
       <Header />
       <Profile />
+      <TechScroll />
       <Footer />
     </main>
   );

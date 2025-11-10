8dfc5f7409f4d9560da740c43a663293f6997175
EmanuProds <emanuprods@proton.me>
2025-11-10 16:14:41 -0300

    import profile component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/layout/Header";
 import Profile from "@/components/ui/Profile";
 import TechScroll from "@/components/ui/TechScroll";
 import About from "@/components/ui/About";
+import Projects from "@/components/ui/Projects";
 import Academic from "@/components/ui/Academic";
 import Footer from "@/components/layout/Footer";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Profile />
       <TechScroll />
       <About />
+      <Projects />
       <Academic />
       <Footer />
     </main>

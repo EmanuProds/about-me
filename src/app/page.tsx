72fcc4ee53200cacef4f668ce52cf2a946886bcd
EmanuProds <emanuprods@proton.me>
2025-11-10 12:18:34 -0300

    refatoring focusing on clean code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
-import Header from "@/components/Header";
-import Profile from "@/components/Profile";
-import TechScroll from "@/components/TechScroll";
-import About from "@/components/About";
-import Projects from "@/components/Projects";
-import Academic from "@/components/Academic";
-import Footer from "@/components/Footer";
+import Header from "@/components/layout/Header";
+import Profile from "@/components/ui/Profile";
 
 export default function Home() {
   return (
-    <main>
+    <main className="pt-20">
       <Header />
       <Profile />
-      <TechScroll />
-      <About />
-      <Projects />
-      <Academic />
-      <Footer />
     </main>
   );
 }

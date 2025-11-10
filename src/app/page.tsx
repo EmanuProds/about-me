313fc86531d5e7bfa01b29968d9beccf35b2552d
EmanuProds <emanuprods@proton.me>
2025-11-10 12:57:44 -0300

    import footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/layout/Header";
 import Profile from "@/components/ui/Profile";
+import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
     <main className="pt-20">
       <Header />
       <Profile />
+      <Footer />
     </main>
   );
 }
